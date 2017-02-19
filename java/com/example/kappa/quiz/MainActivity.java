624856a69acdeaa3e387a728e927ea1361e1e835
KappaSkadoosh <teohcycy95@gmail.com>
2017-02-19 15:47:34 +0800

    Update MainActivity.java

--- java/com/example/kappa/quiz/MainActivity.java
+++ java/com/example/kappa/quiz/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
     int[] myImageList = new int[]{R.drawable.img1, R.drawable.img2, R.drawable.img3,R.drawable.img4,
             R.drawable.img5,R.drawable.img6,R.drawable.img7,R.drawable.img8,R.drawable.img9,
             R.drawable.img10,R.drawable.img11,R.drawable.img12,R.drawable.img13,R.drawable.img14,
-            R.drawable.img15,R.drawable.img16,R.drawable.img17,R.drawable.img17};
+            R.drawable.img15,R.drawable.img16,R.drawable.img17,R.drawable.wk};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
